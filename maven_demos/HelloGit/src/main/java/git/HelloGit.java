eb39ea1ac6f73eeef05b6bc0f25160ceac5d5d68
lili-li <905572538@qq.com>
2021-01-20 19:46:28 +0800

    添加文件

---
+++ maven_demos/HelloGit/src/main/java/git/HelloGit.java
@@ -0,0 +1,17 @@
+package git;
+
+/**
+ * @BelongsProject: maven_demos
+ * @BelongsPackage: git
+ * @Author: LiYing
+ * @CreateTime: 2021-01-20 17:05
+ * @Description:
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Git");
+    }
+
+}
